456afa90e646680e1a061079f183926fe3b8321f
Robson-Moroni <73721160+Robson-Moroni@users.noreply.github.com>
2022-03-02 20:18:05 -0300

    arquivo do projeto

---
+++ src/main/java/com/sistema/apptreino/ApptreinoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.apptreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/apptreino/ApptreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.apptreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
