ebea6170147f5779cf10f819811dfe393fc9a0f6
RubensJTP <rubensneto@jtpsolution.com.br>
2022-03-07 08:56:22 -0300

    dev

--- src/main/java/com/sistema/apptreino/security/SecurityConfig.java
+++ src/main/java/com/sistema/apptreino/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
-		.antMatchers("/assets/**");
+		.antMatchers("/assets/css/**");
 
 	}
 	
