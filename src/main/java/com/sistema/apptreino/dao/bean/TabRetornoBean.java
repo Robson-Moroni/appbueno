2ad67f7f53ede0b349748076348aaf17dc116ba8
RubensJTP <rubensneto@jtpsolution.com.br>
2022-03-19 23:19:33 -0300

    dev...

---
+++ src/main/java/com/sistema/apptreino/dao/bean/TabRetornoBean.java
@@ -0,0 +1,18 @@
+package com.sistema.apptreino.dao.bean;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+public class TabRetornoBean {
+
+    /* 0 - Erro ~ 1 - Sucesso*/
+    private Integer cdStatus;
+
+    private String txEndPointRetorno;
+
+}
--- src/main/java/com/sistema/apptreino/modulos/administrador/cadastros/exercicios/controller/TabCadastrosController.java
+++ src/main/java/com/sistema/apptreino/modulos/administrador/cadastros/exercicios/controller/TabCadastrosController.java
@@ -6,18 +6,24 @@ import com.sistema.apptreino.dao.TabConjuntoGrupoMuscularObj;
 import com.sistema.apptreino.dao.TabDivisaoObj;
 import com.sistema.apptreino.dao.TabGrupoMuscularObj;
 import com.sistema.apptreino.dao.TabNivelTreinoObj;
+import com.sistema.apptreino.dao.bean.TabRetornoBean;
 import com.sistema.apptreino.modulos.administrador.cadastros.exercicios.service.TabDivisaoService;
 import com.sistema.apptreino.modulos.administrador.cadastros.exercicios.service.TabNivelTreinoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sistema.apptreino.TagConstants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cadastro")
@@ -51,18 +57,45 @@ public class TabCadastrosController {
 	}
 
 	private void carregarListas(ModelAndView mv) {
-		mv.addObject("listNivelTreino", tabNivelTreinoService.listar());
 		mv.addObject("listDivisao", tabDivisaoService.listar());
 	}
 
 
 
-	@PostMapping("/gravar/nivelTreino")
+/*	@PostMapping("/gravar/nivelTreino")
 	public ModelAndView gravarNivelTreino(@Validated TabNivelTreinoObj tabNivelTreinoObj, HttpServletRequest httpServletRequest, Errors errors){
 		ModelAndView mv = new ModelAndView(txUrlTela);
 
 		tabNivelTreinoService.gravar(tabNivelTreinoObj);
 		return mv;
+	}*/
+
+	@GetMapping("/carregarListas/listarNivelTreino")
+	public ModelAndView listarNivelTreino(){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("listNivelTreino", tabNivelTreinoService.listar());
+		return mv;
+	}
+
+	@PostMapping("/gravar/nivelTreino")
+	public @ResponseBody List<?> gravarNivelTreino(@Validated TabNivelTreinoObj tabNivelTreinoObj, HttpServletRequest httpServletRequest, Errors erros){
+		List<TabRetornoBean> error = new ArrayList<TabRetornoBean>();
+		try {
+			tabNivelTreinoService.gravar(tabNivelTreinoObj);
+			List<TabRetornoBean> success = new ArrayList<TabRetornoBean>();
+			TabRetornoBean sucesso = new TabRetornoBean();
+			sucesso.setCdStatus(1);
+			sucesso.setTxEndPointRetorno("/cadastro/carregarListas/listarNivelTreino");
+			success.add(sucesso);
+			return success;
+
+		}
+		catch (Exception ex) {
+			TabRetornoBean erro = new TabRetornoBean();
+			erro.setCdStatus(0);
+			error.add(erro);
+			return error;
+		}
 	}
 
 	@PostMapping("/gravar/divisao")
